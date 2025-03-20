7b584474aed33515fd6fe8903ae42ee540101d35
technicalguftgu <meghanar19@gmail.com>
2025-03-20 21:35:06 +0000

    Added Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,29 @@
 const express = require("express");
+const sequelize = require("./config/database"); // Correct import
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const { sequelize } = require("./models");
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express();
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
 
-const authRoutes = require("./routes/auth");
-const postRoutes = require("./routes/posts");
-const commentRoutes = require("./routes/comments");
+// Test database connection
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected"))
+  .catch(err => console.error("Unable to connect to the database:", err));
 
-app.use("/auth", authRoutes);
-app.use("/posts", postRoutes);
-app.use("/comments", commentRoutes);
+// Sync database
+sequelize
+  .sync()
+  .then(() => console.log("Database synced"))
+  .catch(err => console.error("Unable to sync database:", err));
 
-app.listen(5000, async () => {
-  console.log("Server running on port 5000");
-  await sequelize.authenticate();
-  console.log("Database connected");
-});
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/categories", categoryRoutes);
+
+app.listen(5000, () => console.log("Server running on http://localhost:5000"));
