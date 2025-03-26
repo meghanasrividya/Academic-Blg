64a486f9a39427784e789fd7b1a45b98e4ab996c
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-03-26 00:27:42 +0000

    updated create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './CreatePost.css';
 
 export default function CreatePost() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const navigate = useNavigate();
 
-  const handleCreate = async e => {
+  const handleCreate = async (e) => {
     e.preventDefault();
     try {
       await axios.post('/api/posts', { title, content });
@@ -19,25 +20,29 @@ export default function CreatePost() {
   };
 
   return (
-    <div className="form-page">
-      <form onSubmit={handleCreate} className="post-form">
-        <h2>Create Post</h2>
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-          required
-        />
-        <textarea
-          placeholder="Content"
-          value={content}
-          onChange={e => setContent(e.target.value)}
-          rows="8"
-          required
-        />
-        <button type="submit">Publish</button>
-      </form>
-    </div>
+    <>
+      <div className='page-wrapper'>
+        <div className='form-page'>
+          <form onSubmit={handleCreate} className='post-form'>
+            <h2>Create Post</h2>
+            <input
+              type='text'
+              placeholder='Title'
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+            <textarea
+              placeholder='Content'
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              rows='8'
+              required
+            />
+            <button type='submit'>Publish</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
